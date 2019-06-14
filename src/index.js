d8377a7c1aa5304960b5a06e042bb6f3d367aef4
auxibackup <auxibackup@outlook.com>
2019-06-13 17:11:01 -0700

    edit index.js: correct import location

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MovieApp from './components/MovieApp';
+import MovieApp from './components/MovieApp/MovieApp';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<MovieApp />, document.getElementById('root'));
