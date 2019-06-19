51d7a5f824960a2a7e1d73a0255a797ee2e98ae6
Javiercerna <javier.cerna@pucp.pe>
2019-06-18 23:00:21 -0500

    Add Trailer component

--- src/stories/index.jsx
+++ src/stories/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import { storiesOf } from '@storybook/react';
+import { themes } from '@storybook/theming';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import MovieCard from '../components/MovieCard/MovieCard';
+import Trailer from '../components/Trailer/Trailer';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
@@ -26,4 +28,33 @@ storiesOf('MovieCard', module)
       movieTitle="Avengers Endgame"
       imgSource="https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg"
     />
+  ));
+
+storiesOf('Trailer', module)
+  .addParameters({ options: { theme: themes.dark } })
+  .add('basic trailer', () => (
+    <Trailer
+      trailerThumbnail="https://i.ytimg.com/vi/ZFy8ZgLd574/hqdefault.jpg"
+      movieTitle="Child's Play (2019 film)"
+    />
+  ))
+  .add('multiple trailers, one over the other', () => (
+    <div>
+      <Trailer
+        trailerThumbnail="https://i.ytimg.com/vi/ZFy8ZgLd574/hqdefault.jpg"
+        movieTitle="Child's Play (2019 film)"
+      />
+      <Trailer
+        trailerThumbnail="http://media.altpress.com/uploads/2019/01/spider-man-far-from-home-trailer-1.jpg"
+        movieTitle="Spiderman Far From Home"
+      />
+      <Trailer
+        trailerThumbnail="https://i.ytimg.com/vi/7TavVZMewpY/maxresdefault.jpg"
+        movieTitle="The Lion King"
+      />
+      <Trailer
+        trailerThumbnail="https://i.ytimg.com/vi/5Lxu75r3-kI/maxresdefault.jpg"
+        movieTitle="Hobbs & Shaw"
+      />
+    </div>
   ));
