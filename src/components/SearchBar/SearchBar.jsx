458cadeade967cc88de9a9a07a7f36a1fac42312
auxibackup <auxibackup@outlook.com>
2019-06-25 09:39:54 -0700

    Edit SearchBar.jsx: remove comment code

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,32 +2,6 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './SearchBar.css';
 
-// class SearchBar extends Component {
-//     updateList = this.props.updateList;
-
-//     // updates the titles to search specific titles.
-//     update(e)  {
-//         if(e) e.preventDefault();
-
-//         let [input] = e.target.children;
-
-//         if(input.value) this.updateList(input.value);
-//     }
-
-//     render() {
-//         return (
-//             <div className='form-container'>
-//                 <form onSubmit={this.update.bind(this)}>
-//                     <input type='text' placeholder='Search' />
-//                     <button id='search-button' type='submit'></button>
-//                 </form>
-//             </div>
-//         )
-//     }
-// }
-
-// export default SearchBar;
-
 const SearchBar = ({ updateList }) => {
   const handleSubmit = (event) => {
     event.preventDefault();
