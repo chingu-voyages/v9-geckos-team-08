06ecf93fce775ed67595e71c644c8c5122063d5c
Javiercerna <javier.cerna@pucp.pe>
2019-06-29 17:33:31 -0500

    Add AliceCarousel for MovieCards

--- src/components/MovieCards/MovieCards.jsx
+++ src/components/MovieCards/MovieCards.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import AliceCarousel from 'react-alice-carousel';
 import MovieCard from '../MovieCard/MovieCard';
 import * as TMDB from '../../utils/MoviesAPI';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import './MovieCards.css';
 
 const MovieCards = ({ titles }) => {
@@ -9,16 +11,30 @@ const MovieCards = ({ titles }) => {
     ? title.substr(0, title.indexOf(':'))
     : title);
 
+  const items = titles.map(title => (
+    <MovieCard
+      className="card"
+      key={title.id}
+      movieTitle={shortenTitle(title.title)}
+      imgSource={TMDB.getPosterURL(title.poster_path)}
+    />
+  ));
+
+  const responsive = {
+    0: { items: 1 },
+    576: { items: 2 },
+    768: { items: 2 },
+    992: { items: 3 },
+    1200: { items: 5 },
+  };
+
   return (
     <div className="latest-section">
-      {titles.map(title => (
-        <MovieCard
-          className="card"
-          key={title.id}
-          movieTitle={shortenTitle(title.title)}
-          imgSource={TMDB.getPosterURL(title.poster_path)}
-        />
-      ))}
+      <AliceCarousel
+        items={items}
+        responsive={responsive}
+        mouseDragEnabled
+      />
     </div>
   );
 };
