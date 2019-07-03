1cbcbd6e1cd285ed5ca929ddebe21dcab0849bc8
Javiercerna <javier.cerna@pucp.pe>
2019-07-03 11:14:51 -0500

    Add youtube videos href to each Trailer component

--- src/components/Trailer/Trailer.test.jsx
+++ src/components/Trailer/Trailer.test.jsx
@@ -4,6 +4,6 @@ import Trailer from './Trailer';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<Trailer trailerThumbnail="https://i.ytimg.com/vi/ZFy8ZgLd574/hqdefault.jpg" movieTitle="Child's Play (2019 film)" />, div);
+  ReactDOM.render(<Trailer trailerThumbnail="https://i.ytimg.com/vi/ZFy8ZgLd574/hqdefault.jpg" movieTitle="Child's Play (2019 film)" videos={[]} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
